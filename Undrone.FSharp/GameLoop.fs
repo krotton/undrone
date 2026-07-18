1c3a3a8d5acf4f2020ace0f2a33b2fec1a3e8c9f
Chris Otto <krotton@gmail.com>
2026-07-19 00:15:35 +0200

    Remove mutable fields from GameLoop using closure capturing

--- Undrone.FSharp/GameLoop.fs
+++ Undrone.FSharp/GameLoop.fs
@@ -4,28 +4,12 @@ open Godot
 
 type GameLoop(node: Node2D) =
     let confirmDialog = new ConfirmationDialog(Title = "Exit Game", DialogText = "Are you sure you want to exit?")
-    let mutable mainMenu : MainMenu option = None
-    let mutable gameWorld : GameWorld option = None
 
     member this.OnExitConfirmed() =
         node.GetTree().Quit()
 
     member this.OnExitRequested() =
         confirmDialog.PopupCentered()
-
-    member this.OnNewGame() =
-        // Remove Main Menu
-        match mainMenu with
-        | Some menu ->
-            menu.QueueFree()
-            mainMenu <- None
-        | None -> ()
-
-        // Instantiate and add GameWorld scene
-        let world = new GameWorld()
-        gameWorld <- Some world
-        node.AddChild(world)
-        world.Initialize()
 
     member this.Ready() =
         // Setup Exit Confirmation Dialog
@@ -36,13 +20,19 @@ type GameLoop(node: Node2D) =
         let canvasLayer = new CanvasLayer()
         node.AddChild(canvasLayer)
 
-        // Instantiate and add MainMenu with delegates to GameLoop methods
-        let onNewGame = System.Action(this.OnNewGame)
+        // Instantiate and add MainMenu with delegates.
+        // We use a closure here to capture canvasLayer, allowing us to free the menu
+        // and transition to GameWorld without maintaining mutable fields in GameLoop.
+        let onNewGame = System.Action(fun () ->
+            canvasLayer.QueueFree()
+            let world = new GameWorld()
+            node.AddChild(world)
+            world.Initialize()
+        )
         let onExit = System.Action(this.OnExitRequested)
         let menu = new MainMenu(onNewGame, onExit)
         menu.SetAnchorsPreset(Control.LayoutPreset.FullRect)
         canvasLayer.AddChild(menu)
-        mainMenu <- Some menu
 
     member this.Process(delta: double) =
         // Game loop ticks will go here
