c0e83524d13f3a4187665c8b6cd6405b5e2c6ecd
Chris Otto <krotton@gmail.com>
2026-07-18 23:27:29 +0200

    Replace raster logo.jpg with vector logo.svg (transparent background, infinite scaling)

--- Undrone.FSharp/Library.fs
+++ Undrone.FSharp/Library.fs
@@ -4,7 +4,7 @@ open Godot
 
 type GameLoop(node: Node2D) =
     member this.Ready() =
-        let texture = GD.Load<Texture2D>("res://assets/logo.jpg")
+        let texture = GD.Load<Texture2D>("res://assets/logo.svg")
         let sprite = new Sprite2D()
         sprite.Texture <- texture
         sprite.Position <- Vector2(576.0f, 324.0f)
