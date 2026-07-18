d8a0dbe6ae59db1c538c2a9eaf5549c1367e4e0c
Chris Otto <krotton@gmail.com>
2026-07-18 23:00:27 +0200

    Display 'Undrone' text label on the game screen via F#

--- Undrone.FSharp/Library.fs
+++ Undrone.FSharp/Library.fs
@@ -5,6 +5,11 @@ open Godot
 type GameLoop(node: Node2D) =
     member this.Ready() =
         GD.Print("Hello from F# in Godot!")
+        let label = new Label()
+        label.Text <- "Undrone"
+        label.Position <- Vector2(150.0f, 150.0f)
+        label.AddThemeFontSizeOverride("font_size", 64)
+        node.AddChild(label)
 
     member this.Process(delta: double) =
         // Game loop ticks will go here
